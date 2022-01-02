9a73e16dd7cfe8921456a4a85a590eb54c9c6c23
Aytaç <79279282+aytcp@users.noreply.github.com>
2022-01-02 22:14:00 +0300

    Create Python Temel.py

---
+++ Python Temel.py
@@ -0,0 +1,9 @@
+#Bir listeyi düzleştiren (flatten) fonksiyon yazın. Elemanları birden çok katmanlı listelerden ([[3],2] gibi) oluşabileceği gibi, non-scalar verilerden de oluşabilir.
+
+#input: [[1,'a',['cat'],2],[[[3]],'dog'],[4],[5]]
+
+a = [[1,'a',['cat'],2],[[[3]],'dog'],[4],[5]]
+for l in a:
+    print(l)
+flatten = [e for l in a for e in l]
+print(flatten)
